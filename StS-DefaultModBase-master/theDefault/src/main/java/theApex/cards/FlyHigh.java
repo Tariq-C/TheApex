f5c782325fd5c28e3d68152e4123e15df36d6f49
Tariq-C <tariqchatur@gmail.com>
2020-09-15 15:35:16 -0700

    Sept 15th initial push Cards Added: Ambush Defend_A FlyHigh HuntTheHunter NightStalker

---
+++ StS-DefaultModBase-master/theDefault/src/main/java/theApex/cards/FlyHigh.java
@@ -0,0 +1,93 @@
+package theApex.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.actions.common.ExhaustAction;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import theApex.TheApexMod;
+import theApex.characters.TheApex;
+
+import static theApex.TheApexMod.makeCardPath;
+
+public class FlyHigh extends AbstractDynamicCard {
+
+    /*
+     * "Hey, I wanna make a bunch of cards now." - You, probably.
+     * ok cool my dude no problem here's the most convenient way to do it:
+     *
+     * Copy all of the code here (Ctrl+A > Ctrl+C)
+     * Ctrl+Shift+A and search up "file and code template"
+     * Press the + button at the top and name your template whatever it is for - "AttackCard" or "PowerCard" or something up to you.
+     * Read up on the instructions at the bottom. Basically replace anywhere you'd put your cards name with SplinterArrow
+     * And then you can do custom ones like 16 and Enemy if you want.
+     * I'll leave some comments on things you might consider replacing with what.
+     *
+     * Of course, delete all the comments and add anything you want (For example, if you're making a skill card template you'll
+     * likely want to replace that new DamageAction with a gain Block one, and add baseBlock instead, or maybe you want a
+     * universal template where you delete everything unnecessary - up to you)
+     *
+     * You can create templates for anything you ever want to. Cards, relics, powers, orbs, etc. etc. etc.
+     */
+
+    // TEXT DECLARATION
+
+    public static final String ID = TheApexMod.makeID(FlyHigh.class.getSimpleName()); // USE THIS ONE FOR THE TEMPLATE;
+    public static final String IMG = makeCardPath("Skill.png");// "public static final String IMG = makeCardPath("SplinterArrow.png");
+    // This does mean that you will need to have an image with the same NAME as the card in your image folder for it to run correctly.
+
+
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.SELF;  //   since they don't change much.
+    private static final CardType TYPE = CardType.SKILL;       //
+    public static final CardColor COLOR = TheApex.Enums.COLOR_GRAY;
+
+    private static final int COST = -1;  // COST = 2
+    private static final int UPGRADED_COST = -1; // UPGRADED_COST = 2
+
+    private static final int BLOCK = 8;
+
+    // /STAT DECLARATION/
+
+
+    public FlyHigh() { // - This one and the one right under the imports are the most important ones, don't forget them
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        baseBlock = block = BLOCK;
+        this.exhaust = true;
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        if (energyOnUse < EnergyPanel.totalCount) {
+            energyOnUse = EnergyPanel.totalCount;
+        }
+        p.energy.use(EnergyPanel.totalCount);
+        for (int i = 0; i < energyOnUse; i++) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new GainBlockAction(p, block));
+        }
+    }
+
+
+    // Upgraded stats.
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            this.exhaust = false;
+            initializeDescription();
+        }
+    }
+}
